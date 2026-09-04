7f9935c4404d6f506ec2621001fe3dcecc7d470e
Ghilea <tougent@gmail.com>
2026-09-04 15:45:58 +0200

    wip

--- INSTALL_ALDORIA_ITEM_RESTYLE_V3.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-set SCRIPT_DIR=%~dp0
-node "%SCRIPT_DIR%content-packs\aldoria-item-restyle-v3\install.mjs" --repo "%CD%" --package-root "%SCRIPT_DIR%content-packs\aldoria-item-restyle-v3" %*
--- INSTALL_ALDORIA_ITEM_RESTYLE_V3.ps1
+++
@@ -1,3 +0,0 @@
-$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$PackageRoot = Join-Path $ScriptDir "content-packs\aldoria-item-restyle-v3"
-node (Join-Path $PackageRoot "install.mjs") --repo (Get-Location).Path --package-root $PackageRoot @args
